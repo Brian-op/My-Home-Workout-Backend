474e90d5c553dee9b9bfe944329346cb25919f2d
Brian <brian.kiprono8@student.moringaschool.com>
2025-06-26 02:27:02 +0300

    workout model

---
+++ server/models/workout.py
@@ -0,0 +1,19 @@
+from server.models import db
+
+class Workout (db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key= True)
+    title= db.Column(db.String(50), nullable=False)
+    date= db.Column(db.Date, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable= False)
+
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "title": self.title,
+            "date": self.date.isoformat(),
+            "user_id": self.user_id,
+            "exercises": [workex.to_dict() for workex in self.workout_exercises]
+        }
